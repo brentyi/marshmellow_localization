21e79f31aee4b7038724c554c5fa872cb926ea24
Brent Yi <yibrenth@gmail.com>
2017-11-28 12:29:51 -0800

    getting there

--- src/filter.cpp
+++ src/filter.cpp
@@ -1,23 +1,13 @@
 #include <ros/ros.h>
-// PCL specific includes
-#include <pcl/io/pcd_io.h>
-#include <pcl/console/time.h>
-//header for filtering
+#include <tf/transform_broadcaster.h>
+#include <sensor_msgs/PointCloud2.h>
+
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/filters/extract_indices.h>
-#include <pcl/features/normal_3d.h>
-//header for clustering purpose
 #include <pcl/segmentation/conditional_euclidean_clustering.h>
-//header for subscribing to kinect
-#include <sensor_msgs/PointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
-//header for model coeffs-->optional
-#include <pcl/ModelCoefficients.h>
-#include <pcl/sample_consensus/model_types.h>
-#include <pcl/sample_consensus/method_types.h>
-//header planar segmentation
 #include <pcl/segmentation/sac_segmentation.h>
 #include <pcl/segmentation/extract_clusters.h>
 #include <pcl/filters/passthrough.h>
@@ -25,16 +15,13 @@
 #include <pcl/filters/project_inliers.h>
 #include <pcl/surface/convex_hull.h>
 #include <pcl/filters/radius_outlier_removal.h>
+#include <pcl/common/centroid.h>
 
 ros::Publisher pub;
 ros::Publisher coef_pub, ind_pub;
 
 void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input){
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZ>);
-  pcl::PointCloud<pcl::PointXYZ> voxel_filtered;
-  pcl::PointCloud<pcl::PointXYZ> cloud_segmented;
-  pcl::PointCloud<pcl::PointXYZ> in_future;
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
 
   pcl::fromROSMsg(*input, *cloud);
 
@@ -43,13 +30,14 @@ void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input){
   pass.setInputCloud (cloud);
   pass.setFilterFieldName ("z");
   pass.setFilterLimits (0.0, 1.8);
-  pass.filter (*cloud_filtered);
+  pass.filter (*cloud);
 
   // Downsample + clean up data a bit
   pcl::VoxelGrid<pcl::PointXYZ> vg;
-  vg.setInputCloud(cloud_filtered);
-  vg.setLeafSize(0.01, 0.01, 0.01);
-  vg.filter(*cloud_filtered);
+  vg.setInputCloud(cloud);
+  vg.setLeafSize(0.0075, 0.0075, 0.0075);
+  vg.setMinimumPointsNumberPerVoxel(10);
+  vg.filter(*cloud);
 
   // Planar segmentation
   pcl::SACSegmentation<pcl::PointXYZ> seg;
@@ -60,22 +48,17 @@ void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input){
   seg.setMethodType(pcl::SAC_RANSAC);
   seg.setOptimizeCoefficients(true);
   seg.setMaxIterations(1000);
-  seg.setDistanceThreshold(0.01);
+  seg.setDistanceThreshold(0.02);
 
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_table(new pcl::PointCloud<pcl::PointXYZ> ());
 
   // Segment the largest planar component from the remaining cloud
-  seg.setInputCloud (cloud_filtered);
+  seg.setInputCloud (cloud);
   seg.segment (*inliers, *coefficients);
-  if (inliers->indices.size () == 0)
-  {
-    std::cout << "Could not estimate a planar model for the given dataset." << std::endl;
-    // break;
-  }
 
   // Extract the planar inliers from the input cloud
   pcl::ExtractIndices<pcl::PointXYZ> extract;
-  extract.setInputCloud (cloud_filtered);
+  extract.setInputCloud (cloud);
   extract.setIndices (inliers);
   extract.setNegative (false);
   extract.filter (*cloud_table);
@@ -83,41 +66,30 @@ void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input){
 
   // Remove the planar inliers, extract the rest
   extract.setNegative (true);
-  extract.filter (*cloud_filtered);
+  extract.filter (*cloud);
 
-  // Find clusters
-  std::vector<pcl::PointIndices> cluster_indices;
-  pcl::EuclideanClusterExtraction<pcl::PointXYZ> ec;
-  ec.setClusterTolerance(0.02); // 2cm
-  ec.setMinClusterSize(100);
-  ec.setMaxClusterSize(25000);
-  ec.setInputCloud(cloud_table);
-  ec.extract(cluster_indices);
+  // Isolate the table (assuming it's the largest cluster in our plane)
+  std::vector<pcl::PointIndices> plane_cluster_indices;
+  pcl::EuclideanClusterExtraction<pcl::PointXYZ> plane_ec;
+  plane_ec.setClusterTolerance(0.02); // 2cm
+  plane_ec.setMinClusterSize(100);
+  plane_ec.setMaxClusterSize(25000);
+  plane_ec.setInputCloud(cloud_table);
+  plane_ec.extract(plane_cluster_indices);
 
-  // Find largest cluster
-  int largest_cluster_size = 0;
-  int largest_idx = 0;
-  for (int i=0; i < cluster_indices.size(); i++) {
-    int cluster_size = cluster_indices[i].indices.size();
-    if (cluster_size > largest_cluster_size) {
-      largest_cluster_size = cluster_size;
+  uint16_t largest_idx = 0;
+  for (int i=1; i < plane_cluster_indices.size(); i++) {
+    if (plane_cluster_indices[i].indices.size() > plane_cluster_indices[largest_idx].indices.size()) {
       largest_idx = i;
     }
   }
 
-  // Keep largest cluster
-  pcl::PointIndices::Ptr largest_cluster(new pcl::PointIndices);
-  largest_cluster->indices = cluster_indices[largest_idx].indices;
+  pcl::PointIndices::Ptr largest_cluster(new pcl::PointIndices());
+  largest_cluster->indices = plane_cluster_indices[largest_idx].indices;
   extract.setInputCloud(cloud_table);
   extract.setIndices(largest_cluster);
   extract.setNegative(false);
   extract.filter(*cloud_table);
-
-  // pcl::RadiusOutlierRemoval<pcl::PointXYZ> outrem;
-  // outrem.setInputCloud(cloud_table);
-  // outrem.setRadiusSearch(0.2);
-  // outrem.setMinNeighborsInRadius(15);
-  // outrem.filter (*cloud_table);
 
   // Project the model inliers
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_projected(new pcl::PointCloud<pcl::PointXYZ>);
@@ -127,7 +99,7 @@ void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input){
   proj.setModelCoefficients (coefficients);
   proj.filter (*cloud_projected);
 
-  // Create a Concave Hull representation of the projected inliers
+  // Isolate objects on the table
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_hull(new pcl::PointCloud<pcl::PointXYZ>);
   pcl::ConvexHull<pcl::PointXYZ> chull;
   chull.setDimension(2);
@@ -135,38 +107,51 @@ void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input){
   chull.reconstruct(*cloud_hull);
 
   pcl::ExtractPolygonalPrismData<pcl::PointXYZ> prism;
-  prism.setInputCloud(cloud_filtered);
+  prism.setInputCloud(cloud);
   prism.setInputPlanarHull(cloud_hull);
   prism.setHeightLimits(0, 0.1);
   prism.segment(*inliers);
 
-  // pcl::ExtractIndices<pcl::PointXYZ> extract;
-  extract.setInputCloud (cloud_filtered);
+  extract.setInputCloud (cloud);
   extract.setIndices (inliers);
   extract.setNegative (false);
-  extract.filter (*cloud_filtered);
+  extract.filter (*cloud);
 
-  // cloud_filtered = cloud_projected;
-  // Publish the model coefficients
-  // pcl_msgs::ModelCoefficients ros_coefficients;
-  // pcl_conversions::fromPCL(coefficients, ros_coefficients);
-  // coef_pub.publish(ros_coefficients);
+  // Remove isolated points (probably noise)
+  // pcl::RadiusOutlierRemoval<pcl::PointXYZ> outrem;
+  // outrem.setInputCloud(cloud);
+  // outrem.setRadiusSearch(0.03);
+  // outrem.setMinNeighborsInRadius(5);
+  // outrem.filter(*cloud);
 
-  // Publish the Point Indices
-  // pcl_msgs::PointIndices ros_inliers;
-  // pcl_conversions::fromPCL(*inliers, ros_inliers);
-  // ind_pub.publish(ros_inliers);
+  // static tf::TransformBroadcaster br;
+  // std::string frame = input->header.frame_id;
 
-  // Create the filtering object
-  // pcl::ExtractIndices<pcl::PointXYZ> extract;
-  // extract.setInputCloud(cloud);
-  // extract.setIndices(inliers);
-  // extract.setNegative(false);
-  // extract.filter(cloud_segmented);
+  // Search for marshmallows
+  std::vector<pcl::PointIndices> mm_cluster_indices;
+  pcl::EuclideanClusterExtraction<pcl::PointXYZ> mm_ec;
+  mm_ec.setClusterTolerance(0.02); // 2cm
+  mm_ec.setMinClusterSize(20);
+  mm_ec.setMaxClusterSize(1000);
+  mm_ec.setInputCloud(cloud);
+  mm_ec.extract(mm_cluster_indices);
+
+  for (int i=0; i < mm_cluster_indices.size(); i++) {
+    if (mm_cluster_indices[i].indices.size() < 10)
+      break;
+
+    pcl::CentroidPoint<pcl::PointXYZ> centroid;
+    for (int j = 0; j < mm_cluster_indices[i].indices.size(); j++) {
+      centroid.add(cloud->points[mm_cluster_indices[i].indices[j]]);
+    }
+    pcl::PointXYZ c;
+    centroid.get(c);
+    std::cout << c.x << "|" << c.y << "|" << c.z << std::endl;
+  }
 
   //Publish the new cloud
   sensor_msgs::PointCloud2 output;
-  pcl::toROSMsg(*cloud_filtered, output);
+  pcl::toROSMsg(*cloud, output);
   output.header.frame_id = input->header.frame_id;
   output.header.stamp=ros::Time::now();
   pub.publish(output);
